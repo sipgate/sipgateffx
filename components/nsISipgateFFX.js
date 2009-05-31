008247166191ee185493861d0814b6b95388e398
Michael Rotmanov <leachim2k@leachim2k.de>
2009-05-31 03:15:49 +0200

    remastered. included xmlrpc.js to component

--- components/nsISipgateFFX.js
+++ components/nsISipgateFFX.js
@@ -1,31 +1,32 @@
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+Components.utils.import("resource://sipgateffx/xmlrpc.js");
 
 function SipgateFFX() {
 	this.wrappedJSObject = this;
-	this.samuraiAuth = {"hostname" : "chrome://sipgateffx",
-			"formSubmitURL" : null,
-			"httprealm" : 'sipgate Account Login',
-			"username" : null,
-			"password" : null
-			};	
+	this.samuraiAuth = {
+		"hostname": "chrome://sipgateffx",
+		"formSubmitURL": null,
+		"httprealm": 'sipgate Account Login',
+		"username": null,
+		"password": null
+	};
 }
 
 SipgateFFX.prototype = {
-  classDescription: "sipgateFFX javascript XPCOM Component",
-  classID:          Components.ID("{BCC44C3C-B5E8-4566-8556-0D9230C7B4F9}"),
-  contractID:       "@api.sipgate.net/sipgateffx;1",
-  QueryInterface: XPCOMUtils.generateQI(), //[Components.interfaces.nsISipgateFFX]),
-  
+	classDescription: "sipgateFFX javascript XPCOM Component",
+	classID: Components.ID("{BCC44C3C-B5E8-4566-8556-0D9230C7B4F9}"),
+	contractID: "@api.sipgate.net/sipgateffx;1",
+	QueryInterface: XPCOMUtils.generateQI(),
 	get password() {
-		if(this.samuraiAuth.password) {
+		if (this.samuraiAuth.password) {
 			return this.samuraiAuth.password;
 		} else {
 			return null;
 		}
 	},
-    
+	
 	get username() {
-		if(this.samuraiAuth.username) {
+		if (this.samuraiAuth.username) {
 			return this.samuraiAuth.username;
 		} else {
 			return null;
@@ -34,50 +35,46 @@ SipgateFFX.prototype = {
 	
 	oPrefService: Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch),
 	
-	setPref: function (aName, aValue, aType) {
+	setPref: function(aName, aValue, aType) {
 		dump(aName);
-	  try{
-	    if(aType == "bool") {
-	      this.oPrefService.setBoolPref(aName, aValue);
-			} else if(aType == "int") {
-	      this.oPrefService.setIntPref(aName, aValue);
-			} else if(aType == "char") {
-	      this.oPrefService.setCharPref(aName, aValue);
+		try {
+			if (aType == "bool") {
+				this.oPrefService.setBoolPref(aName, aValue);
+			} else if (aType == "int") {
+				this.oPrefService.setIntPref(aName, aValue);
+			} else if (aType == "char") {
+				this.oPrefService.setCharPref(aName, aValue);
 			}
-		this.log("preference '"+aName+"' ("+aType+") set to '"+aValue+"'");
-	  }
-	  catch(e){
-		dump(e);
-	  }
+			this.log("preference '" + aName + "' (" + aType + ") set to '" + aValue + "'");
+		} catch (e) {
+			dump(e);
+		}
 	},
 	
-	getPref: function (aName, aType) {
-	  try{
-	    var result;
-	    if(aType == "int") {
-	      result = this.oPrefService.getIntPref(aName);
-	    } else if(aType == "char") {
-	      result = this.oPrefService.getCharPref(aName);
-	    } else if(aType == "bool") {
-	      result = this.oPrefService.getBoolPref(aName);
-	    }
-	    return result;
-	  }
-	  catch(e){
-	    if(aType == "int") {
-	      return 0;
-	    }
-	    else if(aType == "char") {
-	      return null;
-	    }
-	  }
-	  return null;
-	},	
+	getPref: function(aName, aType) {
+		try {
+			var result;
+			if (aType == "int") {
+				result = this.oPrefService.getIntPref(aName);
+			} else if (aType == "char") {
+				result = this.oPrefService.getCharPref(aName);
+			} else if (aType == "bool") {
+				result = this.oPrefService.getBoolPref(aName);
+			}
+			return result;
+		} catch (e) {
+			if (aType == "int") {
+				return 0;
+			} else if (aType == "char") {
+				return null;
+			}
+		}
+		return null;
+	},
 	
 	getSamuraiAuth: function() {
 		// Login Manager exists so this is Firefox 3
-		var passwordManager = Components.classes["@mozilla.org/login-manager;1"]
-					.getService(Components.interfaces.nsILoginManager);
+		var passwordManager = Components.classes["@mozilla.org/login-manager;1"].getService(Components.interfaces.nsILoginManager);
 		
 		// Find users for the given parameters
 		var logins = passwordManager.findLogins({}, this.samuraiAuth.hostname, this.samuraiAuth.formSubmitURL, this.samuraiAuth.httprealm);
@@ -95,41 +92,40 @@ SipgateFFX.prototype = {
 		
 		this.samuraiAuth.username = username;
 		this.samuraiAuth.password = password;
-		return {'username': username, 'password': password};
+		return {
+			'username': username,
+			'password': password
+		};
 	},
 	
 	setSamuraiAuth: function(username, password) {
 		try {
-		   // Get Login Manager 
-		   var passwordManager = Components.classes["@mozilla.org/login-manager;1"]
-								 .getService(Components.interfaces.nsILoginManager);
-		 
-		   // Find users for this extension 
-		   var logins = passwordManager.findLogins({}, this.samuraiAuth.hostname, this.samuraiAuth.formSubmitURL, this.samuraiAuth.httprealm);
-			  
-		   dump("Found passwords count: " + logins.length + "\n");
-		   for (var i = 0; i < logins.length; i++) {
-			passwordManager.removeLogin(logins[i]);
-		   }
-		   
-		   var nsLoginInfo = new Components.Constructor("@mozilla.org/login-manager/loginInfo;1",
-							     Components.interfaces.nsILoginInfo,
-							     "init");
-	
-		   var loginInfo = new nsLoginInfo(this.samuraiAuth.hostname, this.samuraiAuth.formSubmitURL, this.samuraiAuth.httprealm, username, password, "", "");
-		   passwordManager.addLogin(loginInfo);	
-		   
+			// Get Login Manager 
+			var passwordManager = Components.classes["@mozilla.org/login-manager;1"].getService(Components.interfaces.nsILoginManager);
+			
+			// Find users for this extension 
+			var logins = passwordManager.findLogins({}, this.samuraiAuth.hostname, this.samuraiAuth.formSubmitURL, this.samuraiAuth.httprealm);
+			
+			dump("Found passwords count: " + logins.length + "\n");
+			for (var i = 0; i < logins.length; i++) {
+				passwordManager.removeLogin(logins[i]);
+			}
+			
+			var nsLoginInfo = new Components.Constructor("@mozilla.org/login-manager/loginInfo;1", Components.interfaces.nsILoginInfo, "init");
+			
+			var loginInfo = new nsLoginInfo(this.samuraiAuth.hostname, this.samuraiAuth.formSubmitURL, this.samuraiAuth.httprealm, username, password, "", "");
+			passwordManager.addLogin(loginInfo);
+			
+		} catch (ex) {
+			// This will only happen if there is no nsILoginManager component class
+			dump(ex);
 		}
-		catch(ex) {
-		   // This will only happen if there is no nsILoginManager component class
-		   dump(ex);
-		}		
 	}
 	
 };
 var components = [SipgateFFX];
 
 function NSGetModule(compMgr, fileSpec) {
-  return XPCOMUtils.generateModule(components);
+	return XPCOMUtils.generateModule(components);
 }
 
--- content/xmlrpc.js
+++ content/xmlrpc.js
@@ -2,28 +2,24 @@
 XMLRPC Utility code.
 ------------------------------------------------------
 */
-var bfXMLRPC = new Object();
+dump("XMLRPC Library loaded\n");
+var EXPORTED_SYMBOLS = ["bfXMLRPC", "PffXmlHttpReq"];
 
-bfXMLRPC.makeXML = function(method, myParams, isAtom) {
-    if(!isAtom){
+var bfXMLRPC = {
+	makeXML: function(method, myParams) {
         var xml = <methodCall></methodCall>;
         xml.methodName = method;
         //i->0 is the URL
         for(var i=1; i < myParams.length; i++){
-            xml.params.param += <param> <value> { bfXMLRPC.convertToXML(myParams[i], isAtom) }</value> </param>;
+            xml.params.param += <param> <value> { this.convertToXML(myParams[i]) }</value> </param>;
         }
 		
         var theBlogCharType = "UTF-8";
 		
         return '<?xml version="1.0" encoding="' + theBlogCharType + '" ?>' + xml.toXMLString();
-    }
+	},
 	
-    return 0;
-}
-
-bfXMLRPC.convertToXML = function(myParams, isAtom) {
-    //gPFFTempObject = myParams;
-    if(!isAtom){
+	convertToXML: function(myParams) {
         var paramType = myParams.constructor.name;
         var paramTemp = null;
         switch (paramType){
@@ -51,7 +47,7 @@ bfXMLRPC.convertToXML = function(myParams, isAtom) {
             break;
             
           case "Date": //Date Object: var date = new Date();
-            var theDate = bfXMLRPC.iso8601Format(myParams).toString();
+            var theDate = this.iso8601Format(myParams).toString();
             var theErrorString = "NaNNaNNaNTNaN:NaN:NaN";
             if(theDate != theErrorString){
                 paramTemp = "<dateTime.iso8601>" + theDate + "Z</dateTime.iso8601>";
@@ -66,7 +62,7 @@ bfXMLRPC.convertToXML = function(myParams, isAtom) {
             for(var i = 0; i < myParams.length; i++)
             {
                 //dump("\n i: " + i + "\n")
-                tempVal += "<value>" + bfXMLRPC.convertToXML(myParams[i]) + "</value>";
+                tempVal += "<value>" + this.convertToXML(myParams[i]) + "</value>";
             }
             tempVal += "</data></array>";
             paramTemp = tempVal;
@@ -80,13 +76,13 @@ bfXMLRPC.convertToXML = function(myParams, isAtom) {
 				if (typeof myParams[x] != 'undefined') {
 	                if(myParams[x].constructor.name == 'String'){
 	                    if(x == "bits"){
-	                        tempVal += "<member><name>" + x + "</name>" + "<value><base64><![CDATA[" +bfXMLRPC.convertToXML(myParams[x]) + "]]></base64></value>" +"</member>";
+	                        tempVal += "<member><name>" + x + "</name>" + "<value><base64><![CDATA[" +this.convertToXML(myParams[x]) + "]]></base64></value>" +"</member>";
 	                    }else{
-	                        tempVal += "<member><name>" + x + "</name>" + "<value><string><![CDATA[" +bfXMLRPC.convertToXML(myParams[x]) + "]]></string></value>" +"</member>";
+	                        tempVal += "<member><name>" + x + "</name>" + "<value><string><![CDATA[" +this.convertToXML(myParams[x]) + "]]></string></value>" +"</member>";
 	                    }
                     
 	                }else if(myParams[x].constructor.name == 'Date'){
-	                    var theDate = bfXMLRPC.iso8601Format(myParams[x]).toString();
+	                    var theDate = this.iso8601Format(myParams[x]).toString();
 	                    var theErrorString = "NaNNaNNaNTNaN:NaN:NaN";
 	                    if(theDate != theErrorString){
 	                        tempVal += "<member><name>" + x + "</name>" + "<value>" +"<dateTime.iso8601>" + theDate + "Z</dateTime.iso8601>" + "</value>" +"</member>";
@@ -96,12 +92,12 @@ bfXMLRPC.convertToXML = function(myParams, isAtom) {
                     
 	                }else if(myParams[x].constructor.name == 'Number'){
 	                    if( myParams[x] == parseInt(myParams[x]) ){
-	                        tempVal += "<member><name>" + x + "</name>" + "<value>" +"<int>"  +bfXMLRPC.convertToXML(myParams[x]) + "</int>" + "</value>" +"</member>";
+	                        tempVal += "<member><name>" + x + "</name>" + "<value>" +"<int>"  +this.convertToXML(myParams[x]) + "</int>" + "</value>" +"</member>";
 	                    }else{
-	                        tempVal += "<member><name>" + x + "</name>" + "<value>" + "<double>" + bfXMLRPC.convertToXML(myParams[x]) + "</double>" + "</value>" +"</member>";
+	                        tempVal += "<member><name>" + x + "</name>" + "<value>" + "<double>" + this.convertToXML(myParams[x]) + "</double>" + "</value>" +"</member>";
 	                    }
 	                }else{
-	                    tempVal += "<member><name>" + x + "</name>" + "<value>" +bfXMLRPC.convertToXML(myParams[x]) + "</value>" +"</member>";
+	                    tempVal += "<member><name>" + x + "</name>" + "<value>" +this.convertToXML(myParams[x]) + "</value>" +"</member>";
 	                }
 				}
             }
@@ -115,132 +111,131 @@ bfXMLRPC.convertToXML = function(myParams, isAtom) {
             
         }
             
-        //var paramObject = new XML("<string>" + myParams +"</string>");
         var paramObject = new XML(paramTemp);
         
         return paramObject;
-    }
-    
-    return 0;
-}
 
-//XMLToObject is derived from GPL code originally by Flock Inc:
-//For the original source, see: http://cvs-mirror.flock.com/index.cgi/mozilla/browser/components/flock/xmlrpc/content/xmlrpchelper.js?rev=1.1&content-type=text/vnd.viewcvs-markup
-bfXMLRPC.XMLToObject = function(xml, nodeName) {
-    try{
-        if (xml.nodeKind()) {
-            //foo
-        }
-    }catch(e){
-        //foo
-        xml = new XML(xml);
-    }
-    //gPFFTempObject = xml;
-    
-    if (xml.nodeKind() == 'text') {
-            // the default type in string
-            return xml.toString();
-    }
-        
-    switch (xml.name().toString()) {
-        case 'int':
-        case 'i4':
-            return parseInt (xml.text());
-        case 'boolean':
-            return (parseInt (xml.text()) == 1);
-        case 'string':
-            return (xml.text().toString());
-        case 'double':
-            return parseFloat (xml.text());
-        case 'dateTime.iso8601':
-            var val = xml.text().toString().replace(/^\s+|\s+$/g, "");
-
-			if (val.match(/z/i) || (nodeName && nodeName == 'date_created_gmt')) {
-	            val = val.replace(/\-/gi, "");
-	            val = val.replace(/\z/gi, "");
-	            var dateutc =  Date.UTC(val.slice(0, 4), val.slice(4, 6) - 1, 
-	                    val.slice(6, 8), val.slice(9, 11), val.slice(12, 14), 
-	                    val.slice(15,17));
-				return new Date(dateutc);
-			}
-			else {
-				// Check for a timezone offset
-				var possibleOffset = val.substr(-6);
-				var hasTimezone = false;
-				var minutes = null;
-				
-				if (possibleOffset.charAt(0) == "-" || possibleOffset.charAt(0) == "+") {
-					var hours = parseInt(possibleOffset.substr(1,2), 10);
-					var minutes = (hours * 60) + parseInt(possibleOffset.substr(4,2), 10);
+	},
+	
+	//XMLToObject is derived from GPL code originally by Flock Inc:
+	//For the original source, see: http://cvs-mirror.flock.com/index.cgi/mozilla/browser/components/flock/xmlrpc/content/xmlrpchelper.js?rev=1.1&content-type=text/vnd.viewcvs-markup
+	XMLToObject: function(xml, nodeName) {
+	    try{
+	        if (xml.nodeKind()) {
+	            //foo
+	        }
+	    }catch(e){
+	        //foo
+	        xml = new XML(xml);
+	    }
+	    //gPFFTempObject = xml;
+	    
+	    if (xml.nodeKind() == 'text') {
+	            // the default type in string
+	            return xml.toString();
+	    }
+	        
+	    switch (xml.name().toString()) {
+	        case 'int':
+	        case 'i4':
+	            return parseInt (xml.text());
+	        case 'boolean':
+	            return (parseInt (xml.text()) == 1);
+	        case 'string':
+	            return (xml.text().toString());
+	        case 'double':
+	            return parseFloat (xml.text());
+	        case 'dateTime.iso8601':
+	            var val = xml.text().toString().replace(/^\s+|\s+$/g, "");
+	
+				if (val.match(/z/i) || (nodeName && nodeName == 'date_created_gmt')) {
+		            val = val.replace(/\-/gi, "");
+		            val = val.replace(/\z/gi, "");
+		            var dateutc =  Date.UTC(val.slice(0, 4), val.slice(4, 6) - 1, 
+		                    val.slice(6, 8), val.slice(9, 11), val.slice(12, 14), 
+		                    val.slice(15,17));
+					return new Date(dateutc);
+				}
+				else {
+					// Check for a timezone offset
+					var possibleOffset = val.substr(-6);
+					var hasTimezone = false;
+					var minutes = null;
 					
-					if (possibleOffset.charAt(0) == "+") {
-						minutes *= -1;
+					if (possibleOffset.charAt(0) == "-" || possibleOffset.charAt(0) == "+") {
+						var hours = parseInt(possibleOffset.substr(1,2), 10);
+						var minutes = (hours * 60) + parseInt(possibleOffset.substr(4,2), 10);
+						
+						if (possibleOffset.charAt(0) == "+") {
+							minutes *= -1;
+						}
+						
+						hasTimezone = true;
 					}
 					
-					hasTimezone = true;
+					val = val.replace(/\-/gi, "");
+					var dateutc =  Date.UTC(
+						val.slice(0, 4), 
+						val.slice(4, 6) - 1, 
+						val.slice(6, 8), 
+						val.slice(9, 11), 
+						val.slice(12, 14), 
+						val.slice(15,17));
+					dateutc = new Date(dateutc);
+					
+					if (!hasTimezone) {
+						minutes = new Date(dateutc).getTimezoneOffset();
+					}
+					
+					var offsetDate = dateutc.getTime();
+					offsetDate += (1000 * 60 * minutes);
+					dateutc.setTime(offsetDate);
+					return dateutc;
 				}
-				
-				val = val.replace(/\-/gi, "");
-				var dateutc =  Date.UTC(
-					val.slice(0, 4), 
-					val.slice(4, 6) - 1, 
-					val.slice(6, 8), 
-					val.slice(9, 11), 
-					val.slice(12, 14), 
-					val.slice(15,17));
-				dateutc = new Date(dateutc);
-				
-				if (!hasTimezone) {
-					minutes = new Date(dateutc).getTimezoneOffset();
-				}
-				
-				var offsetDate = dateutc.getTime();
-				offsetDate += (1000 * 60 * minutes);
-				dateutc.setTime(offsetDate);
-				return dateutc;
-			}
-        case 'array':
-            var arr = new Array ();
-            for (var i=0; i<xml.data.value.length(); i++) {
-                arr.push (bfXMLRPC.XMLToObject(xml.data.value[i].children()[0]));
-            }
-            return arr;
-        case 'struct':
-            var struct = new Object ();
-            for (var i=0; i < xml.member.length(); i++) {
-                struct[xml.member[i].name.text()] = 
-                    bfXMLRPC.XMLToObject(xml.member[i].value.children()[0], xml.member[i].name.text());
-            }
-            return struct;
-        case 'base64':
-			return (atob(xml.text().toString()));
-        default:
-			// SFLOG("Error parsing XML: " + xml.name().toString());
-            //dump('error parsing XML');
-    }
-}
-
-//Function is derived from GPL code originally by Flock Inc:
-//For more informationm See:
-bfXMLRPC.iso8601Format = function(date) 
-{
-    var datetime = date.getUTCFullYear();
-    var month = String(date.getUTCMonth() + 1);
-    datetime += (month.length == 1 ?  '0' + month : month);
-    var day = date.getUTCDate();
-    datetime += (day < 10 ? '0' + day : day);
-
-    datetime += 'T';
-
-    var hour = date.getUTCHours();
-    datetime += (hour < 10 ? '0' + hour : hour) + ':';
-    var minutes = date.getUTCMinutes();
-    datetime += (minutes < 10 ? '0' + minutes : minutes) + ':';
-    var seconds = date.getUTCSeconds();
-    datetime += (seconds < 10 ? '0' + seconds : seconds);
+	        case 'array':
+	            var arr = new Array ();
+	            for (var i=0; i<xml.data.value.length(); i++) {
+	                arr.push (this.XMLToObject(xml.data.value[i].children()[0]));
+	            }
+	            return arr;
+	        case 'struct':
+	            var struct = new Object ();
+	            for (var i=0; i < xml.member.length(); i++) {
+	                struct[xml.member[i].name.text()] = 
+	                    this.XMLToObject(xml.member[i].value.children()[0], xml.member[i].name.text());
+	            }
+	            return struct;
+	        case 'base64':
+				return (atob(xml.text().toString()));
+	        default:
+				// SFLOG("Error parsing XML: " + xml.name().toString());
+	            //dump('error parsing XML');
+	    }
+	},
 	
-	return datetime;
-}
+	//Function is derived from GPL code originally by Flock Inc:
+	//For more informationm See:
+	iso8601Format: function(date) 
+	{
+	    var datetime = date.getUTCFullYear();
+	    var month = String(date.getUTCMonth() + 1);
+	    datetime += (month.length == 1 ?  '0' + month : month);
+	    var day = date.getUTCDate();
+	    datetime += (day < 10 ? '0' + day : day);
+	
+	    datetime += 'T';
+	
+	    var hour = date.getUTCHours();
+	    datetime += (hour < 10 ? '0' + hour : hour) + ':';
+	    var minutes = date.getUTCMinutes();
+	    datetime += (minutes < 10 ? '0' + minutes : minutes) + ':';
+	    var seconds = date.getUTCSeconds();
+	    datetime += (seconds < 10 ? '0' + seconds : seconds);
+		
+		return datetime;
+	}
+	
+};
 
 /*
     Replace all Stupid XML Requests with this object
@@ -270,82 +265,80 @@ function PffXmlHttpReq( aUrl, aType, aContent, aDoAuthBool, aUser, aPass) {
     this.doAuth = aDoAuthBool;
     //this.callback = aCallBackFunc;
     this.request = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance(Components.interfaces.nsIXMLHttpRequest);
-}
+	
+	this.prepCall = function () {
+	    if(this.doAuth){
+	        this.request.open(this.posttype, this.url, true, this.username, this.password);
+	        
+	        //Keeps stupid Authentication window from poping up
+	        this.request.channel.notificationCallbacks = {
+	            QueryInterface: function (iid) { // nsISupports
+	                if (iid.equals (Components.interfaces.nsISupports) ||
+	                    iid.equals (Components.interfaces.nsIAuthPrompt) ||
+	                    iid.equals (Components.interfaces.nsIInterfaceRequestor)) {
+	                        return this;
+	                }
+	                throw Components.results.NS_ERROR_NO_INTERFACE;
+	            },
+	            getInterface: function (iid, result) { // nsIInterfaceRequestor
+	                if (iid.equals (Components.interfaces.nsIAuthPrompt)) {
+	                    return this;
+	                }
+	                Components.returnCode = Components.results.NS_ERROR_NO_INTERFACE;
+	                return null;
+	            },
+	            prompt: function (dialogTitle, text, passwordRealm, savePAssword, 
+	                    defaultText, result) { // nsIAuthPromptInterface
+	                return false;
+	            },
+	            promptUsernameAndPassword: function (dialogTitle, text, 
+	                    passwordRealm, savePassword, user, pwd) {
+	                          //Didn't work, asking for password again
+	                return false;
+	            },
+	            promptPassword: function (dialogTitle, text, passwordRealm, savePassword, user) {
+	                return false;
+	            }
+	        }
+	    }else{
+	        this.request.open(this.posttype, this.url, true);
+	    }
+	    
+	    var request = this.request;
+	    var onResult = this.onResult;
+	    var onError = this.onError;
+	    this.request.onreadystatechange = function () {
+	        if(request.readyState == 4){ 
+	            if (request.status < 300) {
+	                onResult(request.responseText, request.responseXML);
+	            }else{
+	                try{
+	                    onError(request.statusMessage, request.responseText);
+	                } catch(e) { alert("Error running onError: " + e); }
+	            } 
+	        }
+	    }
+	    
+	    if(this.posttype.toLowerCase() == "post"){
+	        try{
+	            this.request.setRequestHeader('Content-Length', this.content.length );
+	        }catch(e){}
+	    }
+	};
+	
+	this.makeCall = function () {
+	    this.request.send(this.content)
+	};
 
-PffXmlHttpReq.prototype.prepCall = function () {
-    
-    if(this.doAuth){
-        this.request.open(this.posttype, this.url, true, this.username, this.password);
-        
-        //Keeps stupid Authentication window from poping up
-        this.request.channel.notificationCallbacks = {
-            QueryInterface: function (iid) { // nsISupports
-                if (iid.equals (Components.interfaces.nsISupports) ||
-                    iid.equals (Components.interfaces.nsIAuthPrompt) ||
-                    iid.equals (Components.interfaces.nsIInterfaceRequestor)) {
-                        return this;
-                }
-                throw Components.results.NS_ERROR_NO_INTERFACE;
-            },
-            getInterface: function (iid, result) { // nsIInterfaceRequestor
-                if (iid.equals (Components.interfaces.nsIAuthPrompt)) {
-                    return this;
-                }
-                Components.returnCode = Components.results.NS_ERROR_NO_INTERFACE;
-                return null;
-            },
-            prompt: function (dialogTitle, text, passwordRealm, savePAssword, 
-                    defaultText, result) { // nsIAuthPromptInterface
-                return false;
-            },
-            promptUsernameAndPassword: function (dialogTitle, text, 
-                    passwordRealm, savePassword, user, pwd) {
-                          //Didn't work, asking for password again
-                return false;
-            },
-            promptPassword: function (dialogTitle, text, passwordRealm, savePassword, user) {
-                return false;
-            }
-        }
-    }else{
-        this.request.open(this.posttype, this.url, true);
-    }
-    
-    var request = this.request;
-    var onResult = this.onResult;
-    var onError = this.onError;
-    this.request.onreadystatechange = function () {
-        if(request.readyState == 4){ 
-            if (request.status < 300) {
-                onResult(request.responseText, request.responseXML);
-            }else{
-                try{
-                    onError(request.statusMessage, request.responseText);
-                } catch(e) { alert("Error running onError: " + e); }
-            } 
-        }
-    }
-    
-    if(this.posttype.toLowerCase() == "post"){
-        try{
-            this.request.setRequestHeader('Content-Length', this.content.length );
-        }catch(e){}
-    }
+	/*
+	    Defined by Inheritor
+	*/
+	this.onError = function (message) {
+	    //foo
+	};
+	
+	this.onResult = function (aTestRes, aXMLRes) {
+	    //foo
+	};
+	
 }
-
-PffXmlHttpReq.prototype.makeCall = function () {
-    this.request.send(this.content)
-}
-   
-/*
-    Defined by Inheritor
-*/
-
-PffXmlHttpReq.prototype.onError = function (message) {
-    //foo
-}
-
-PffXmlHttpReq.prototype.onResult = function (aTestRes, aXMLRes) {
-    //foo
-}
-
